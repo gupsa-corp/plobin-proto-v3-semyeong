1855d4d8e04f29986cf15e7ae49d67075c000c48
윤정한 <165637494+jeonghanyun@users.noreply.github.com>
2025-09-04 21:20:20 +0900

    뷰 디렉토리 및 라우트 설정을 개선하여 코드의 재사용성을 높였습니다. 기존의 하드코딩된 라우트를 config 파일에서 불러오는 방식으로 변경하였고, 관련 주석을 정리했습니다.

---
+++ config/routes-web.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    '/' => '101-landing-home',
+    '/login' => '201-auth-login',
+    '/signup' => '202-auth-signup',
+    '/dashboard' => '301-service-dashboard',
+    '/admin' => '901-admin-dashboard',
+    '/admin/users' => '902-admin-users',
+    '/admin/settings' => '903-admin-settings',
+];
--- routes/web.php
+++ routes/web.php
@@ -2,33 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-// 101 ~ 199 랜딩 페이지
-Route::get('/', function () {
-    return view('101-landing-home.index');
-});
+$routes = config('routes-web');
 
-// 201 ~ 299 인증 페이지
-Route::get('/login', function () {
-    return view('201-auth-login.index');
-});
-Route::get('/signup', function () {
-    return view('202-auth-signup.index');
-});
-
-// 301 ~ 399 서비스 페이지
-Route::get('/dashboard', function () {
-    return view('301-service-dashboard.index');
-});
-
-// 900 ~ 999 관리자 페이지
-Route::get('/admin', function () {
-    return view('901-admin-dashboard.index');
-});
-
-Route::get('/admin/users', function () {
-    return view('902-admin-users.index');
-});
-
-Route::get('/admin/settings', function () {
-    return view('903-admin-settings.index');
-});
+foreach ($routes as $path => $view) {
+    Route::get($path, function () use ($view) {
+        return view($view . '.index');
+    });
+}
