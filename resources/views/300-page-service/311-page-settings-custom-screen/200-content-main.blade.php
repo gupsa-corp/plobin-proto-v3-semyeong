cce76d956f52296ee501bfd2bfbe1378117f4a51
윤정한 <165637494+jeonghanyun@users.noreply.github.com>
2025-09-10 16:47:26 +0900

    커스텀 화면 정보 표시 UI 제거
    주요 변경사항:
    - 선택된 커스텀 화면 정보를 표시하는 UI 요소를 제거하여 코드 간결성을 높였습니다.
    - 불필요한 주석 및 코드 블록을 삭제하여 가독성을 개선했습니다.
    
    UI 정리 및 코드 개선을 위한 작업을 진행했습니다.

--- resources/views/300-page-service/311-page-settings-custom-screen/200-content-main.blade.php
+++ resources/views/300-page-service/311-page-settings-custom-screen/200-content-main.blade.php
@@ -228,26 +228,6 @@ window.currentSandboxType = @json($currentSandboxType ?? '');
                             </div>
                         </div>
                     </div>
-
-                    <!-- 선택된 화면 정보 -->
-                    <div x-show="selectedCustomScreen && selectedCustomScreen !== ''" x-transition>
-                        <div class="bg-blue-50 border border-blue-200 rounded-lg p-4">
-                            <div class="flex">
-                                <div class="flex-shrink-0">
-                                    <svg class="h-5 w-5 text-blue-400" fill="currentColor" viewBox="0 0 20 20">
-                                        <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path>
-                                    </svg>
-                                </div>
-                                <div class="ml-3">
-                                    <h4 class="text-sm font-medium text-blue-800">선택된 커스텀 화면</h4>
-                                    <div class="mt-1 text-sm text-blue-700">
-                                        <span x-text="customScreens.find(s => s.id == selectedCustomScreen)?.title"></span>
-                                        화면이 이 페이지에 적용됩니다.
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 </form>
             </div>
         </div>
