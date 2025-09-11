5fca9fa12316808ec02f250503f54631f5df97ce
third-park <qkrtpaud01@gmail.com>
2025-09-11 13:46:20 +0900

    form publisher 폼 항목 미리보기 시 json파일 대로 폼 생성

--- app/Livewire/FormPublisher/FormPreview.php
+++ app/Livewire/FormPublisher/FormPreview.php
@@ -16,12 +16,124 @@ class FormPreview extends Component
         $this->formId = $formId;
         $this->form = SandboxForm::findOrFail($formId);
         
+        // Convert Form-Creator format to Form-Publisher format
+        $this->convertFormCreatorData();
+        
         // Initialize form data
         if ($this->form->form_fields) {
             foreach ($this->form->form_fields as $field) {
                 $this->formData[$field['id'] ?? $field['name'] ?? ''] = '';
             }
         }
+    }
+    
+    private function convertFormCreatorData()
+    {
+        // Check if form_fields contains Form-Creator format (components with properties)
+        if ($this->form->form_fields && is_array($this->form->form_fields)) {
+            $firstField = $this->form->form_fields[0] ?? null;
+            
+            // If the first field has 'properties' key, it's Form-Creator format
+            if ($firstField && isset($firstField['properties'])) {
+                $convertedFields = [];
+                
+                foreach ($this->form->form_fields as $component) {
+                    $field = $this->convertComponentToField($component);
+                    if ($field) {
+                        $convertedFields[] = $field;
+                    }
+                }
+                
+                // Update the form with converted data
+                $this->form->form_fields = $convertedFields;
+            }
+        }
+    }
+    
+    private function convertComponentToField($component)
+    {
+        $props = $component['properties'] ?? [];
+        $field = [
+            'id' => $component['id'],
+            'type' => $this->mapComponentType($component['type']),
+            'label' => $props['label'] ?? 'Untitled Field',
+            'name' => $props['name'] ?? $component['id'],
+            'required' => $props['required'] ?? false,
+            'disabled' => $props['disabled'] ?? false,
+            'hidden' => $props['hidden'] ?? false,
+            'placeholder' => $props['placeholder'] ?? '',
+            'description' => $props['description'] ?? ''
+        ];
+        
+        switch ($component['type']) {
+            case 'dropdown':
+            case 'select':
+                $field['type'] = 'select';
+                $field['options'] = $this->parseOptions($props['options'] ?? '');
+                break;
+            case 'radiogroup':
+                $field['type'] = 'radio';
+                $field['options'] = $this->parseOptions($props['options'] ?? '');
+                break;
+            case 'checkbox':
+                $field['type'] = 'checkbox';
+                break;
+            case 'textarea':
+                $field['type'] = 'textarea';
+                $field['rows'] = intval($props['rows'] ?? 4);
+                break;
+            case 'input':
+                $field['type'] = $props['inputType'] ?? 'text';
+                break;
+            case 'button':
+                $field['type'] = 'button';
+                $field['text'] = $props['text'] ?? $props['label'] ?? 'Button';
+                $field['buttonType'] = $props['type'] ?? 'button';
+                break;
+            case 'header':
+                $field['type'] = 'header';
+                $field['level'] = $props['level'] ?? 'h2';
+                $field['text'] = $props['text'] ?? $props['label'] ?? 'Header Text';
+                break;
+            case 'tagpicker':
+                $field['type'] = 'tagpicker';
+                $field['options'] = $this->parseOptions($props['options'] ?? '');
+                break;
+            default:
+                $field['type'] = 'text';
+        }
+        
+        return $field;
+    }
+    
+    private function mapComponentType($creatorType)
+    {
+        $typeMap = [
+            'input' => 'text',
+            'textarea' => 'textarea',
+            'dropdown' => 'select',
+            'select' => 'select',
+            'checkbox' => 'checkbox',
+            'radiogroup' => 'radio',
+            'button' => 'button',
+            'header' => 'header',
+            'file' => 'file',
+            'date' => 'date',
+            'tagpicker' => 'tagpicker'
+        ];
+        
+        return $typeMap[$creatorType] ?? 'text';
+    }
+    
+    private function parseOptions($options)
+    {
+        if (is_array($options)) {
+            return $options;
+        }
+        if (is_string($options)) {
+            return array_filter(array_map('trim', explode("\n", $options)));
+        }
+        return [];
     }
     
     public function submitForm()
