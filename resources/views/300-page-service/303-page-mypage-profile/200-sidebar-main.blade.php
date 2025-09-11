7f137da8eb6787ed39ac4832e8bfb932864f5366
윤정한 <165637494+jeonghanyun@users.noreply.github.com>
2025-09-11 09:17:37 +0900

    마이페이지 사이드바의 주석 수정 및 불필요한 코드 제거
    주요 변경사항:
    - 대시보드 사이드바 주석을 마이페이지 사이드바로 수정했습니다.
    - 불필요한 사이드바 조직 정보 포함 코드를 제거하여 가독성을 높였습니다.

--- resources/views/300-page-service/303-page-mypage-profile/200-sidebar-main.blade.php
+++ resources/views/300-page-service/303-page-mypage-profile/200-sidebar-main.blade.php
@@ -10,11 +10,9 @@
     }
 @endphp
 
-{{-- 대시보드 사이드바 --}}
+{{-- 마이페이지 사이드바 --}}
 <nav class="sidebar" style="position: fixed; left: 0; top: 0; width: 240px; height: 100vh; background: #ffffff; border-right: 1px solid #E1E1E4; display: flex; flex-direction: column; z-index: 10; box-sizing: border-box;">
     @include('000-common-assets.100-logo')
-
-    @include('300-page-service.300-common.202-sidebar-organization-info')
 
     @include('300-page-service.300-common.201-sidebar-navigation', ['navItems' => $navItems])
 </nav>
